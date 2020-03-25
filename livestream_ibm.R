6fa37b9cf183ee77c96a32aa93bd9ae4d2fe6228
Brad Duthie <brad.duthie@gmail.com>
2020-03-25 15:58:53 +0000

    Fix error in reproduction and init function to merge new offspring
    Fix an error that caused a crash only one plant capable of reproduction (due dimension issue in R). Initialise a function to add new offspring.

--- livestream_ibm.R
+++ livestream_ibm.R
@@ -32,12 +32,66 @@ competition <- function(P, L_size){
     return(P);
 }
     
+# Produces new seeds and disperse them on the landscape
+# P is the array of parent plants
+# L_size is the x and y dimension of the landscape
+# N_seeds is the number of seeds each parent above thresh_size produces
+# rng is the range within the parent location of seed dispersal
+# thresh_size is the threshold size of producing seeds for a parent
+reproduction <- function(P, L_size, N_seeds = 10, rng = 2, thresh_size = 10){
+    P_sp    <- P[P[,4] > thresh_size, ];
+    N_sp    <- sum(P[,4] > thresh_size);
+    all_of  <- NULL;
+    if(N_sp > 0){
+        for(i in 1:N_sp){
+             if(is.vector(P_sp) == TRUE){
+                 P_x    <- P_sp[2];
+                 P_y    <- P_sp[3];   
+             }else{
+                 P_x    <- P_sp[i, 2];
+                 P_y    <- P_sp[i, 3];
+             }
+             Pxs    <- seq(from = P_x - rng, to = P_x + rng, by = 1);
+             Pys    <- seq(from = P_y - rng, to = P_y + rng, by = 1);
+             p_ID   <- rep(x = 0, times = N_seeds);
+             p_x    <- sample(x = Pxs, size = N_seeds, replace = TRUE);
+             p_y    <- sample(x = Pys, size = N_seeds, replace = TRUE);
+             p_sz   <- rep(x = 1, times = N_seeds);
+             P_off  <- cbind(p_ID, p_x, p_y, p_sz);
+             all_of <- rbind(all_of, P_off);
+        }
+        all_of[all_of[, 2] < 0 | all_of[, 2] > L_size, 4] <- -1; 
+        all_of[all_of[, 3] < 0 | all_of[, 3] > L_size, 4] <- -1; 
+        all_of <- all_of[all_of[,4] > 0, ];
+    }
+    return(all_of);
+}
+
+inc_Os <- function(P, O){
+    st_IDs <- max(P[,1]) + 1;
+    new_ID <- seq(from = st_IDs, to = st_IDs + dim(O)[1] - 1);
+    O[,1]  <- new_ID;
+    P      <- rbind(P, O);
+    return(P);
+}
+
+
+    
+P <- ini_Ps(L_size = 20, N_ini = 50, shp = 5, rat = 1)
+P <- competition(P, L_size = 20)
+O <- reproduction(P = P, L_size = 20, N_seeds = 10, rng = 2, thresh_size = 10)
+if(!is.null(O)){
+    P <- inc_Os(P = P, O = O);
+}
 
 
 
 
 
-P <- ini_Ps(L_size = 10, N_ini = 200);
-P <- competition(P = P, L_size = 10);
+
+plot(x = P_sp[,2], y = P_sp[,3], pch = 16, cex = 2, col = "red", 
+     xlim = c(-1, 11), ylim = c(-1, 11));
+points(x = all_of[,2], y = all_of[,3])
+
 
 
